9cb739623918aa72509b277a537f6448ca2ee6d1
Jeised <davidjs164@gmail.com>
2022-09-11 16:07:29 -0500

    mi primer commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+package t1;
+
+public class Main {
+
+    public static void main(String args[])
+    {
+        EcuacionCuadratica ecuacion = new EcuacionCuadratica();
+        ecuacion.establecerA(2);
+        ecuacion.establecerB(4);
+        ecuacion.establecarC(-6);
+
+        System.out.println("Valor A: "+ecuacion.obtenerA());
+        System.out.println("Valor B: "+ecuacion.obtenerB());
+        System.out.println("Valor C: "+ecuacion.obtenerC());
+
+        ecuacion.discriminante();
+
+
+    }
+}
